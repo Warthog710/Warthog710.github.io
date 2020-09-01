a92b4678690089c58e4e21a70900fb96d2c328ea
Warthog710 <computergeek710@gmail.com>
2020-09-01 16:04:48 -0700

    added addShadow & removeShadow functions

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,13 @@ function test()
 {
     
 }
+
+function addShadow(id)
+{
+    document.getElementById(id).style.boxShadow = "6px 6px 3px grey";
+}
+
+function removeShadow(id)
+{
+    document.getElementById(id).style.boxShadow = "";
+}
