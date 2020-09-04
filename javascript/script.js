421078f4e3306099e2c78ec69d823170a88fe5e1
Warthog710 <computergeek710@gmail.com>
2020-09-04 00:32:30 -0700

    format, navbar, icons, links, scroll spy - updates

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,27 @@
-function getAge()
-{
+function getPgRdy() {
     birthday = new Date(1998, 7, 10);
     var ageDifMs = Date.now() - birthday.getTime();
     var ageDate = new Date(ageDifMs);
-    document.getElementById("age").innerHTML = Math.abs(ageDate.getUTCFullYear() - 1970);     
+    document.getElementById("age").innerHTML = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    //Enable Bootstrap Tooltips
+    $(document).ready(function () {
+        $("body").tooltip({ selector: '[data-toggle=tooltip]' });
+    });
 }
 
-function addShadow(id)
-{
+function addShadow(id) {
     document.getElementById(id).style.boxShadow = "6px 6px 3px grey";
 }
 
-function removeShadow(id)
-{
+function removeShadow(id) {
     document.getElementById(id).style.boxShadow = "";
 }
 
-function enlargeStars(id)
-{
-        document.getElementById(id).style = "transform: scale(1.2, 1.2)";
+function enlargeStars(id) {
+    document.getElementById(id).style = "transform: scale(1.2, 1.2)";
 }
 
-function shrinkStars(id)
-{
-        document.getElementById(id).style = "transform: scale(1.0, 1.0)"; 
+function shrinkStars(id) {
+    document.getElementById(id).style = "transform: scale(1.0, 1.0)";
 }
